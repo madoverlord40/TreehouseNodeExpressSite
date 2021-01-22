f36e591c4d4dd913eead14cb7d1da267b1d13990
jamesh <jamesh39@outlook.com>
2021-01-22 11:29:44 -0800

    updated error handling

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false}));
-//app.use(cookieParser());
+
 app.use('/static', express.static('public'));
 app.use('/images', express.static('public/images'));
 
--- routes/projects.js
+++ routes/projects.js
@@ -4,14 +4,24 @@ const data = require('../data/data.json');
 const projects = data.projects;
 
 router.get( '/', ( req, res ) => {
-
+  //if they try to go to the /projects route without an id, send them back to the main page.
   res.redirect('/');
 });
 
 router.get('/:id', (req, res) => {
     const index = req.params.id;
-    const templateData = projects[index];
-    res.render( `project`, {templateData});
+    //lets make sure we are navigating to a project id that is valid
+    if(index < 0 || index > 4 || index == null) {
+      const err = new Error('Sorry this project index is not valid! range is 0-4');
+      err.status = 404;
+      res.locals.error = err;
+      res.status(err.status);
+      res.render('error');
+    }
+    else {
+      const templateData = projects[index];
+      res.render( `project`, {templateData});
+    }
     
 });
 
